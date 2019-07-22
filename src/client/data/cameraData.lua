73fab7b886fc1c75779e750b0052af63f87c7245
elosant <elosant69@gmail.com>
2019-07-22 09:41:22 +0100

    Adjusted default shake values

--- src/client/data/cameraData.lua
+++ src/client/data/cameraData.lua
@@ -1,7 +1,7 @@
 return {
 	shakeData = { -- Used as default values in the damped sinusoidal.
-		initialAmplitude = 15.2, -- Initial amplitude defines the starting "offset" (multiplied by random noise), the higher the value the larger the effect of the shake.
-		decayConstant = 3.7, -- Acts as the coefficient of time (the exponent of e), describes rate of decay, higher the value the lower the effect of "aftershocks".
+		initialAmplitude = 7, -- Initial amplitude defines the starting "offset" (multiplied by random noise), the higher the value the larger the effect of the shake.
+		decayConstant = 2.7, -- Acts as the coefficient of time (the exponent of e), describes rate of decay, higher the value the lower the effect of "aftershocks".
 		angularFrequency = 1.35, -- Multiplicand of pi*time, how "fast" the sinusodial progresses.
 	}
 }
